3fc76445417e3684f382992586308dc7be1a2e99
ShafiqShams <shafiqshamsessani@gmail.com>
2021-09-04 15:15:12 +0200

    MapType set, Marker callout added with styles.

--- mobileapp/LarvaeOnMap/src/scenes/Map/Map.js
+++ mobileapp/LarvaeOnMap/src/scenes/Map/Map.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Image, Dimensions} from 'react-native';
-import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import MapView, {PROVIDER_GOOGLE, Marker, Callout} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 import styles from './MapStyles';
+import {Button, Text} from 'react-native-paper';
 
 const Map = () => {
   const {width, height} = Dimensions.get('window');
-
   const aspectRatio = width / height;
   const latitude_delta = 0.15;
   const longitude_delta = latitude_delta * aspectRatio;
@@ -15,46 +15,59 @@ const Map = () => {
     latitudeDelta: latitude_delta,
     longitudeDelta: longitude_delta,
   };
-
+  const selectedMapType = "standard";
   const [fishData, setFishData] = useState([]);
   // const [initialRegion, setInitialRegion] = useState(initialRegion);
-  
+
   useEffect(() => {
     //TODO:
     //Fetching from the backend DB
     getData();
-    return () => {}
-  }, [])
-  
-  
+    return () => {};
+  }, []);
+
   const getData = () => {
     const fetchData = require('../../model/trajectories_nostokes_subset_10000_sample_10_lines.json');
     setFishData(fetchData);
-  }
+  };
 
   return (
     <MapView
       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
       style={styles.map}
-      region={initialRegion}>
+      mapType = { selectedMapType }
+      region={initialRegion
+      }>
       {fishData.map((marker, index) => {
-        
         return (
-          <Marker 
-            key={index} 
+          <Marker
+            key={index}
             coordinate={{
               latitude: marker.lat,
               longitude: marker.lon,
-            }} 
-            onPress={()=> console.log("pressed")}
-          >
-          <Image
-            source={require('../../assets/images/fish_marker.png')}
-            style={{width: 28, height: 30}}
-            resizeMode="contain"
-          />
+            }}
+            onPress={() => console.log('pressed')}
+            
+            >
+            <Image
+              source={require('../../assets/images/fish_marker.png')}
+              style={{width: 28, height: 30}}
+              resizeMode="contain"
+            />
+            <Callout tooltip>
+              <View style={styles.textContainer}>
+                <Text style={styles.obsStyle}>
+                    {'Observation ID: ' + marker.obs}
+                  </Text>
+                  <Text style={styles.descStyle}>
+                    {'Trajectory ID: ' + marker.traj}
+                  </Text>
+                  <Text style={styles.timeStyle}>
+                    {'Timestamp: ' + marker.time}
+                  </Text>
+              </View>
+            </Callout>
           </Marker>
-          
         );
       })}
     </MapView>
