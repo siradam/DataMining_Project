584565593349eeda6776a0c26d3bf35290e3ede8
ShafiqShams <shafiqshamsessani@gmail.com>
2021-09-06 17:19:40 +0200

    splashscreen hidden in app.js

--- mobileapp/LarvaeOnMap/App.js
+++ mobileapp/LarvaeOnMap/App.js
@@ -1,10 +1,18 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import MainTabsNavigator from './src/scenes/Tabs/MainTabsNavigator';
+import SplashScreen from 'react-native-splash-screen';
+
 import {
   StatusBar
 } from "react-native";
-function App() {
+
+const App = () => {
+
+  React.useEffect(() => {
+    SplashScreen.hide();
+  });
+
   return (
     <NavigationContainer>
       <StatusBar barStyle = "dark-content" hidden = {false} backgroundColor = "#FFF" translucent = {true}/>
