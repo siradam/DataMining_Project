25aebebf45ea41c86657831a4405ceaf4d06f7bc
ShafiqShams <shafiqshamsessani@gmail.com>
2021-09-13 22:53:48 +0200

    Settings.js updated

--- mobileapp/LarvaeOnMap/src/scenes/Settings/Settings.js
+++ mobileapp/LarvaeOnMap/src/scenes/Settings/Settings.js
@@ -1,18 +1,16 @@
 import React, {useState} from 'react';
 import {View, Text, Image} from 'react-native';
 import styles from './SettingsStyles';
-import {Checkbox, Switch, TextInput} from 'react-native-paper';
+import {Checkbox, Switch} from 'react-native-paper';
 
 import colors from '../../styles/colors';
+import TextWithInputBoxStyles from '../../components/TextWithInputBox/TextWithInputBox';
 const Settings = () => {
   const [checked, setChecked] = useState(false);
   const [isSwitchOn1, setIsSwitchOn1] = useState(true);
   const [isSwitchOn2, setIsSwitchOn2] = useState(false);
   const [isSwitchOn3, setIsSwitchOn3] = useState(true);
   const [isSwitchOn4, setIsSwitchOn4] = useState(true);
-
-  const [obsID, setObsID] = useState('0');
-  const [trajID, setTrajID] = useState('4');
 
   return (
     <View style={styles.sceneWrapper}>
@@ -42,48 +40,9 @@ const Settings = () => {
 
         <View style={styles.separatorStyle} />
 
-        <View style={styles.settingContainer}>
-          <Image
-            style={[styles.iconStyle, {height: 25, width: 25}]}
-            source={require('../../assets/images/list_icon.png')}
-          />
-
-          <Text style={styles.labelStyle}>{'Observation ID'}</Text>
-          <View style={styles.textInputStyleContainer}>
-            <TextInput
-              maxLength={1}
-              theme={{ colors: { primary: colors.blue }}}
-              mode = {'outlined'}
-              value={obsID}
-              onChangeText={obsID => setObsID(obsID)}
-              selectionColor = {colors.black01}
-              underlineColor = {colors.blue}
-              outlineColor = {colors.black01}
-              style={styles.textInputStyle}
-            />
-          </View>
-        </View>
-
-        <View style={styles.settingContainer}>
-          <Image
-            style={[styles.iconStyle, {height: 25, width: 25}]}
-            source={require('../../assets/images/list_icon.png')}
-          />
-          <Text style={styles.labelStyle}>{'Trajectory ID'}</Text>
-          <View style={styles.textInputStyleContainer}>
-            <TextInput
-              maxLength={1}
-              theme={{ colors: { primary: colors.blue }}}
-              mode = {'outlined'}
-              value={trajID}
-              onChangeText={trajID => setTrajID(trajID)}
-              selectionColor = {colors.black01}
-              underlineColor = {colors.blue}
-              outlineColor = {colors.black01}
-              style={styles.textInputStyle}
-            />
-          </View>
-        </View>
+        {/* Text with input boxes for obs and traj ids */}
+        <TextWithInputBoxStyles textLabel={'Observation ID'} val={'0'} />
+        <TextWithInputBoxStyles textLabel={'Trajectory ID'} val={'4'} />
 
         <View style={styles.settingContainer}>
           <Image
