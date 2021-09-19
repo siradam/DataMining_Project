10cb459a9c56364b9ccf6ddb7dabfd69167ec775
ShafiqShams <shafiqshamsessani@gmail.com>
2021-09-19 16:39:47 +0200

    Colors file from styles

---
+++ mobileapp/LarvaeOnMap/src/styles/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+  black01: "#000000",
+  black02: "#2E2E2E",
+  white: "#ffffff",
+  grey01: "#B7B7B7",
+  grey02: "#949494",
+  lightblue: "#4293f5",
+  blue: "#085394"
+};
+
+export default colors;
