99a93a47d027da57027b56fea0f8b98117c2d5b9
ShafiqShams <shafiqshamsessani@gmail.com>
2021-09-19 16:38:48 +0200

    FishMarker Component separted

---
+++ mobileapp/LarvaeOnMap/src/components/FishMarker/FishMarker.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+import {Marker} from 'react-native-maps';
+
+const FishMarker = ({fishData}) => {
+  return (
+    <Marker
+      coordinate={{
+        latitude: fishData.lat,
+        longitude: fishData.lon,
+      }}>
+      <Image
+        source={require('../../assets/images/fish_marker.png')}
+        style={{width: 30, height: 30}}
+        resizeMode="contain"
+      />
+    </Marker>
+  );
+};
+
+export default FishMarker;
